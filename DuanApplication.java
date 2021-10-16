c098df18e948a1fe603e65665ff74eab0659cc66
Nguyễn Viết Nhân <76809905+nhanhala@users.noreply.github.com>
2021-10-16 19:42:28 +0700

    Add files via upload

---
+++ DuanApplication.java
@@ -0,0 +1,13 @@
+package duan.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuanApplication.class, args);
+	}
+
+}
